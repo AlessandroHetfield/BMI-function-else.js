d6147ce4793a21154e9275d97effd1e02b9b9d05
AlessandroHetfield <160076296+AlessandroHetfield@users.noreply.github.com>
2024-05-09 20:55:15 -0400

    Add files via upload

---
+++ 8demayolovecalculator.js
@@ -0,0 +1,13 @@
+var n = Math.random();
+n = n*6;
+n = Math.floor(n) + 1;
+console.log(`${n}`);
+
+//var yourself = //prompt("Write your name");
+///var she= //prompt ("Write her name");
+var x = Math.random();
+x = x*100;
+x = Math.floor(x) + 1;
+console.log(`${x}`);
+
+
